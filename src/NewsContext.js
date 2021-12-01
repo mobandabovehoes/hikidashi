86269177177d65371abf15ce744f9651921cbe2f
mobandabovehoes <49375207+mobandabovehoes@users.noreply.github.com>
2021-12-01 11:34:05 +0900

    Update NewsContext.js

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -6,11 +6,11 @@ export const NewsContext = createContext();
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
   const apiKey = "9793cd5b524345e7923d0a7820669fa6";
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  
   useEffect(() => {
     axios
       .get(
-        `${proxyUrl}https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`
+        `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`
       )
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));

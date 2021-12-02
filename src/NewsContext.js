1ea533fdf80404334d7c3b0939bda1a91a1670ca
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2021-12-02 22:04:35 +0900

    Update NewsContext.js

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+ import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const NewsContext = createContext();
@@ -8,9 +8,8 @@ export const NewsContextProvider = (props) => {
   const apiKey = "9793cd5b524345e7923d0a7820669fa6";
   const proxyUrl = "https://corsanywhere.herokuapp.com/";
 
-  
 
-  useEffect(() => {
+ useEffect(() => {
     axios
       .get(`${proxyUrl}https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`)
       .then((response) => setData(response.data))
@@ -23,9 +22,3 @@ export const NewsContextProvider = (props) => {
     </NewsContext.Provider>
   );
 };
-
-//  <h1 className="news__title">{data.title}</h1>
-//       <p className="news__desc">{data.description}</p>
-//       <span className="news__author">{data.author}</span> <br />
-//       <span className="news__published">{data.publishedAt}</span>
-//       <span className="news__source">{data.source.name}</span>

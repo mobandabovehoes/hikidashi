9257b3fb90b5bcfec3b2c004ec748caae588baa6
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2022-01-29 19:33:58 +0900

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,4 @@
-import React from 'react'
+　import React from 'react'
 import "./books.css";
 import Button from "@mui/material/Button";
 import Box from '@mui/material/Box';
@@ -37,7 +37,7 @@ import Modal from '@mui/material/Modal';
 
          <Button>
            <a href={url} target="_blank" rel="noreferrer noopener">
-             Amazon
+             買う・見る
            </a>
          </Button>
          <Button>
@@ -48,81 +48,4 @@ import Modal from '@mui/material/Modal';
        </div>
      );
    };
- 
-
-
-// const Book = ({ img, title, author, note, url, question }) => {
-//   const [open, setOpen] = React.useState(false);
-//   const handleOpen = () => setOpen(true);
-//   const handleClose = () => setOpen(false);
-//   const Modal = () => (
-//     <Popup
-//       trigger={
-//         <Button className="button" onClick={handleOpen} open={open}>
-//           詳細
-//         </Button>
-//       }
-//       modal
-//     >
-//       {(close) => (
-//         <div className="modal">
-//           <Button className="close" open={open} onClose={handleClose}>
-//             &times;
-//           </Button>
-//           <div className="header">{question}</div>
-//           <div className="content">{note}</div>
-//           <div className="actions">
-//             <Button className="button" open={open} onClose={handleClose} >
-//               閉じる
-//             </Button>
-//           </div>
-//         </div>
-//       )}
-//     </Popup>
-//   );
-
-//   return (
-//     <article
-//       className="book"
-//     >
-//       <img src={img} alt="thumbnail" />
-//       <h1>{title}</h1>
-//       <h4>{author}</h4>
-
-//       <Modal />
-//       <Button type="button">
-//         <a href={url} target="_blank" rel="noreferrer noopener">
-//           Amazon
-//         </a>
-//       </Button>
-//     </article>
-//   );
-// };
-
-// function Books(id, title, img, price, rating) {
-//   return (
-//     <div className="books">
-//       <div className="books__info">
-//         <h5>{title}</h5>
-//         <p className="books__price">
-//           <small>￥</small>
-//           <strong>{price}</strong>
-//         </p>
-
-//         <div className="books__rating">
-//           {Array (rating)
-//             .fill()
-//             .map((_,i) =>(
-//               <p>★</p>
-//             ))}
-//         </div>
-//       </div>
-
-//       <img src={img} alt="" />
-
-
-//     </div>
-//   );
-// }
- 
  

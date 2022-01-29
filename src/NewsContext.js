3698ecd5c85ee721ce4fdc2c0a10231e41e0c341
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2022-01-29 11:51:23 +0900

    Update NewsContext.js

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -6,12 +6,12 @@ export const NewsContext = createContext();
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
   const apiKey = "9793cd5b524345e7923d0a7820669fa6";
-  const proxyUrl = "https://corsanywhere.herokuapp.com/";
+   
 
 
  useEffect(() => {
     axios
-      .get(`${proxyUrl}https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`)
+      .get(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`)
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
   }, []);

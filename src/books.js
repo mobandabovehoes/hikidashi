67cbd721f2a4a83deaae72095debeb5903a1481f
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2022-01-29 19:34:35 +0900

    Update books.js

--- src/books.js
+++ src/books.js
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import Books from "./Books"
-
-// function Book() {
-//   return (
-//     <div className="book">
-//       <div className="book__container">
-//         <Books
-//           id="0"
-//           title="銃・病原菌・鉄　上巻"
-//           price={950}
-//           rating={5}
-//           img="51U6f63ccKL.jpg"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Book
 export const data = [
   {
     id: 0,
@@ -36,6 +16,7 @@ export const data = [
     note: "コンテナって評価されてないよね。でも実はじつ経済を支える最も重要なサプライチェーンの一つ。現在ではマクドナルドのポテトがSサイズだけになっている。それはコンテナがとどかないから。",
     url: "https://www.amazon.co.jp/%E3%82%B3%E3%83%B3%E3%83%86%E3%83%8A%E7%89%A9%E8%AA%9E-%E4%B8%96%E7%95%8C%E3%82%92%E5%A4%89%E3%81%88%E3%81%9F%E3%81%AE%E3%81%AF%E3%80%8C%E7%AE%B1%E3%80%8D%E3%81%AE%E7%99%BA%E6%98%8E%E3%81%A0%E3%81%A3%E3%81%9F-%E5%A2%97%E8%A3%9C%E6%94%B9%E8%A8%82%E7%89%88-%E3%83%9E%E3%83%AB%E3%82%AF%E3%83%BB%E3%83%AC%E3%83%93%E3%83%B3%E3%82%BD%E3%83%B3-ebook/dp/B07Z4DNFG7/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%82%B3%E3%83%B3%E3%83%86%E3%83%8A%E7%89%A9%E8%AA%9E&qid=1636262673&sr=8-1",
     question: "コンテナ物語",
+    youtube: "https://youtu.be/RS5hRV-3NnM",
   },
   {
     id: 2,
@@ -47,6 +28,7 @@ export const data = [
     url: "https://www.amazon.co.jp/%E6%96%87%E5%BA%AB-%E9%8A%83%E3%83%BB%E7%97%85%E5%8E%9F%E8%8F%8C%E3%83%BB%E9%89%84-%EF%BC%88%E4%B8%8A%EF%BC%89-1%E4%B8%873000%E5%B9%B4%E3%81%AB%E3%82%8F%E3%81%9F%E3%82%8B%E4%BA%BA%E9%A1%9E%E5%8F%B2%E3%81%AE%E8%AC%8E-%E8%8D%89%E6%80%9D%E7%A4%BE%E6%96%87%E5%BA%AB-%E3%82%B8%E3%83%A3%E3%83%AC%E3%83%89%E3%83%BB%E3%83%80%E3%82%A4%E3%82%A2%E3%83%A2%E3%83%B3%E3%83%89/dp/4794218788/ref=sr_1_3?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E9%8A%83%E3%83%BB%E7%97%85%E5%8E%9F%E8%8F%8C%E3%83%BB%E9%89%84+1%E4%B8%873000%E5%B9%B4%E3%81%AB%E3%82%8F%E3%81%9F%E3%82%8B%E4%BA%BA%E9%A1%9E%E5%8F%B2%E3%81%AE%E8%AC%8E&qid=1636264659&sr=8-3",
     question:
       "文庫　銃・病原菌・鉄　（上）　1万3000年にわたる人類史の謎 (草思社文庫)",
+    youtube: "https://youtu.be/hypQjJUbk9g",
   },
   {
     id: 3,
@@ -56,8 +38,8 @@ export const data = [
     note: "世界1200万部突破の『サピエンス全史』著者が戦慄の未来を予言する! 『サピエンス全史』は私たちがどこからやってきたのかを示した。『ホモ・デウス』は私たちがどこへ向かうのかを示す。 全世界1200万部突破の『サピエンス全史』の著者が描く、衝撃の未来! 我々は不死と幸福、神性を目指し、 ホモ・デウス(神のヒト)へと自らをアップグレードする。 そのとき、格差は想像を絶するものとなる。",
     url: "https://www.amazon.co.jp/%E3%83%9B%E3%83%A2%E3%83%BB%E3%83%87%E3%82%A6%E3%82%B9-%E4%B8%8A-%E3%83%86%E3%82%AF%E3%83%8E%E3%83%AD%E3%82%B8%E3%83%BC%E3%81%A8%E3%82%B5%E3%83%94%E3%82%A8%E3%83%B3%E3%82%B9%E3%81%AE%E6%9C%AA%E6%9D%A5-%E3%83%A6%E3%83%B4%E3%82%A1%E3%83%AB%E3%83%BB%E3%83%8E%E3%82%A2%E3%83%BB%E3%83%8F%E3%83%A9%E3%83%AA/dp/4309227368/ref=sr_1_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%83%9B%E3%83%A2%E3%83%BB%E3%83%87%E3%82%A6%E3%82%B9%EF%BC%88%E3%83%A6%E3%83%B4%E3%82%A1%E3%83%AB%E3%83%BB%E3%83%8E%E3%82%A2%E3%83%BB%E3%83%8F%E3%83%A9%E3%83%AA%EF%BC%89&qid=1636266885&sr=8-2",
     question: "ホモ・デウス 上: テクノロジーとサピエンスの未来",
+    youtube: "https://youtu.be/J6QaWq8TZYQ",
   },
-  
 ];
 
 export const nakata__data = [
@@ -69,6 +51,7 @@ export const nakata__data = [
     note: "ある一家が借金を背負う。莫大な負債を返済するために命をかけてお金儲けをする。死に際でお金を稼ごうとするとこんなにも頑張れるのかと思う。ドラマなのにドラマではないような錯覚がたまらない。",
     url: "https://www.netflix.com/browse?jbv=80117552",
     question: "オザークへようこそ",
+    youtube: "https://youtu.be/Ep-410JusSc",
   },
-  　
 ];
+
